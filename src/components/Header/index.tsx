2a1be000901372d489c8b30072ac68942865c8ae
4llves <jhonatab4@gmail.com>
2023-10-19 23:59:18 -0300

    create component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './styles'
+import { Timer, Scroll } from 'phosphor-react'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <a href="">
+          <Timer />
+        </a>
+        <a href="">
+          <Scroll />
+        </a>
+      </nav>
+    </HeaderContainer>
+  )
+}
