481522770dad9c5a922059450d5a6f7ea03b3e67
4llves <jhonatab4@gmail.com>
2023-10-24 23:56:04 -0300

    model controlled vs uncontrolled

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,8 +8,11 @@ import {
   StartCoutdownButton,
   TaskInput,
 } from './styles'
+import { useState } from 'react'
 
 export function Home() {
+  const [task, setTask] = useState('')
+
   return (
     <HomeContainer>
       <form action="">
@@ -19,6 +22,8 @@ export function Home() {
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
           />
 
           <datalist id="task-suggestions">
@@ -49,7 +54,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCoutdownButton disabled type="submit">
+        <StartCoutdownButton disabled={!task} type="submit">
           <Play />
           Começar
         </StartCoutdownButton>
