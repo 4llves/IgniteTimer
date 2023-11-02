9fd8bb83c1bf3544325e71f01e2afe2f41a74e95
4llves <jhonatab4@gmail.com>
2023-11-02 19:58:22 -0300

    Convertendo para contexto

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,14 @@ import { Router } from './Router'
 
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { Home } from './Home'
 
 export function App() {
-  return <Home />
-  // return (
-  //   <ThemeProvider theme={defaultTheme}>
-  //     <BrowserRouter>
-  //       <Router />
-  //     </BrowserRouter>
-  //     <GlobalStyle />
-  //   </ThemeProvider>
-  // )
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
